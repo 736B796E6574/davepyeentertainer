45df62d92e262d39f56e44e8f28f132f030f4fdf
Martin McInerney <martin_mcinerney@live.ie>
2024-01-05 21:45:01 +0000

    adds static tags to all hrefs

--- dpentertainer/settings.py
+++ dpentertainer/settings.py
@@ -1,6 +1,12 @@
 import os
 from pathlib import Path
 
+try:
+    from env.py import *
+except ImportError:
+    pass
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
